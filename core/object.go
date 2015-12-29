1a95b726d124b663021b617f457083d10f3566cd
Michal Michalowski <michal.michalowski@adtoma.com>
2015-12-30 00:46:00 +0100

    Add GetId method for HyperObject interface

--- core/object.go
+++ core/object.go
@@ -3,7 +3,11 @@ package core
 // HyperObject is an interface expected to be implemented by the objects
 // that are about to be stored in the hyperdb.
 type HyperObject interface {
-	// GetAttributes returns a slice of all the attributes of the object
+	// GetId returns a unique ID of the object the hyperdb client wants to
+	// use as a reference for the object. If 0 is returned, hyperdb assigns its
+	// own ID.
+	GetId() uint64
+	// GetAttributes returns a slice of all the attributes of the object.
 	GetAttributes() []Attribute
 	// GetData retuns a raw representation of the object.
 	// The client of the hyperdb is responsible for objects serialization.
