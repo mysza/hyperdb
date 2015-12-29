7f2f1053c2e42be59a23a9828936c12f7f79756f
Michal Michalowski <michal.michalowski@adtoma.com>
2015-12-30 00:33:51 +0100

    Add initial database API

--- core/db.go
+++ core/db.go
@@ -10,3 +10,22 @@ type DB struct {
 func Open(path string) (*DB, error) {
 	return nil, nil
 }
+
+// Add adds an object to the given namespace - indexes all attributes and stores data
+// accessible through GetData() method of the HyperObject.
+// The function returns unique id of the object stored in hyperdb that can
+// later be used to update or remove object in/from the namespace.
+func (db *DB) Add(namespace string, obj *HyperObject) (uint64, error) {
+	return nil
+}
+
+// Update updates the object with specified unique id in the given namespace.
+func (db *DB) Update(namespace string, uid uint64, obj *HyperObject) error {
+	return nil
+}
+
+// Remove removes passed object from the given namespace
+// based on the unique id of the object.
+func (db *DB) Remove(namespace string, uid uint64) ([]byte, error) {
+	return nil, nil
+}
--- core/object.go
+++ core/object.go
@@ -1,1 +1,11 @@
 package core
+
+// HyperObject is an interface expected to be implemented by the objects
+// that are about to be stored in the hyperdb.
+type HyperObject interface {
+	// GetAttributes returns a slice of all the attributes of the object
+	GetAttributes() []Attribute
+	// GetData retuns a raw representation of the object.
+	// The client of the hyperdb is responsible for objects serialization.
+	GetData() []byte
+}
