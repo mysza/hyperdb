d9a57db3dab236887fff7703fc4e72225f179ba1
mYsZa (Michał Michałowski) <michal.michalowski@adtoma.com>
2015-12-30 12:53:30 +0100

    Add database open and close functionality

--- db.go
+++ db.go
@@ -1,14 +1,46 @@
 package hyperdb
 
+import (
+	"errors"
+	"github.com/boltdb/bolt"
+	"time"
+)
+
+const defaultDataFileName = "hyper.db"
+
+var (
+	ErrCannotCloseNilDB     = errors.New("attempted to close nil database")
+	ErrCannotCloseNilBoltDB = errors.New("attemted to close nil Bolt database")
+)
+
 // DB represents a collection of objects with multitude of attributes,
 // that can be retrieved based all possible dimensions.
 type DB struct {
+	boltHandle *bolt.DB
 }
 
 // Open creates and opens a database at the given path.
 // If the file doesn't exist, it is created automatically.
 func Open(path string) (*DB, error) {
-	return nil, nil
+	if path == "" {
+		path = defaultDataFileName
+	}
+	db, err := bolt.Open(path, 0600, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		return nil, err
+	}
+	return &DB{boltHandle: db}, nil
+}
+
+// Close closes the database.
+func (db *DB) Close() error {
+	if db == nil {
+		return ErrCannotCloseNilDB
+	}
+	if db.boltHandle == nil {
+		return ErrCannotCloseNilBoltDB
+	}
+	return db.boltHandle.Close()
 }
 
 // Add adds an object to the given namespace - indexes all attributes and stores data
