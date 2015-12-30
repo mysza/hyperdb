f4f3d03ef66b9a1277a34412100d263a7c6e2587
mYsZa (Michał Michałowski) <michal.michalowski@adtoma.com>
2015-12-30 12:34:27 +0100

    Add naive implementation of the HyperObject interface

--- object.go
+++ object.go
@@ -6,10 +6,29 @@ type HyperObject interface {
 	// GetId returns a unique ID of the object the hyperdb client wants to
 	// use as a reference for the object. If 0 is returned, hyperdb assigns its
 	// own ID.
-	GetId() string
+	GetID() string
 	// GetAttributes returns a slice of all the attributes of the object.
 	GetAttributes() []Attribute
 	// GetData retuns a raw representation of the object.
 	// The client of the hyperdb is responsible for objects serialization.
 	GetData() []byte
+}
+
+// HyperObjectBase is a built-in naive implementation of the HyperObject interface.
+type HyperObjectBase struct {
+	ID         string
+	Attributes []Attribute
+	Data       []byte
+}
+
+func (hob HyperObjectBase) GetID() string {
+	return hob.ID
+}
+
+func (hob HyperObjectBase) GetAttributes() []Attribute {
+	return hob.Attributes
+}
+
+func (hob *HyperObjectBase) GetData() []byte {
+	return hob.Data
 }
